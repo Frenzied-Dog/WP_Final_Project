e3fe833038b74006e5ea28de4a02a7a1536d14b6
Hank_TUF <hck134599@gmail.com>
2023-12-01 02:45:29 +0800

    Update LoginForm

---
+++ Final_Project/LoginForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Final_Project {
+    partial class LoginForm {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.TextBox_ID = new System.Windows.Forms.TextBox();
+            this.LoginPicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextBox_ID
+            // 
+            this.TextBox_ID.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox_ID.Location = new System.Drawing.Point(498, 315);
+            this.TextBox_ID.Name = "TextBox_ID";
+            this.TextBox_ID.Size = new System.Drawing.Size(266, 33);
+            this.TextBox_ID.TabIndex = 0;
+            this.TextBox_ID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LoginPicBox
+            // 
+            this.LoginPicBox.BackColor = System.Drawing.Color.Transparent;
+            this.LoginPicBox.Image = global::Final_Project.Properties.Resources.Btn1_2;
+            this.LoginPicBox.Location = new System.Drawing.Point(579, 382);
+            this.LoginPicBox.Name = "LoginPicBox";
+            this.LoginPicBox.Size = new System.Drawing.Size(107, 46);
+            this.LoginPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.LoginPicBox.TabIndex = 1;
+            this.LoginPicBox.TabStop = false;
+            this.LoginPicBox.Click += new System.EventHandler(this.LoginPicBox_Click);
+            this.LoginPicBox.MouseEnter += new System.EventHandler(this.LoginPicBox_MouseEnter);
+            this.LoginPicBox.MouseLeave += new System.EventHandler(this.LoginPicBox_MouseLeave);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Final_Project.Properties.Resources.BackGround_3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 711);
+            this.Controls.Add(this.LoginPicBox);
+            this.Controls.Add(this.TextBox_ID);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.Text = "CK Connect";
+            ((System.ComponentModel.ISupportInitialize)(this.LoginPicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_ID;
+        private System.Windows.Forms.PictureBox LoginPicBox;
+    }
+}
+
---
+++ Final_Project/LoginForm.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Final_Project {
+	public partial class LoginForm : Form {
+		public static IPublicClientApplication PublicClientApp;
+		static string graphAPI_Me = "https://graph.microsoft.com/v1.0/me";
+		string name = "";
+		string ID = "";
+
+		public LoginForm() {
+			InitializeComponent();
+			PublicClientApp = PublicClientApplicationBuilder.Create("9cbbf580-d751-42ec-97d0-01d35df3fa7c")
+				 .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+				 .WithAuthority(AzureCloudInstance.AzurePublic, "c2e7753f-aa05-4abc-8c02-293ad122ca19")
+				 .Build();
+		}
+
+		private void LoginPicBox_MouseEnter(object sender, EventArgs e) {
+			LoginPicBox.Image = Properties.Resources.Btn2_2;
+		}
+
+		private void LoginPicBox_MouseLeave(object sender, EventArgs e) {
+			LoginPicBox.Image = Properties.Resources.Btn1_2;
+		}
+
+		async private void LoginPicBox_Click(object sender, EventArgs e) {
+			ID = TextBox_ID.Text;
+
+			string[] scopes = { "User.Read" }; //User.ReadBasic.All
+			AuthenticationResult authResult = null;
+			var accounts = await PublicClientApp.GetAccountsAsync();
+
+			try {
+				authResult = await PublicClientApp.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
+					.ExecuteAsync();
+
+			} catch (MsalUiRequiredException ex) {
+				try {
+					authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+						.WithPrompt(Prompt.SelectAccount)
+						.WithLoginHint($"{ID}@ncku.edu.tw")
+						.ExecuteAsync();
+
+				} catch (MsalException msalex) {
+					MessageBox.Show($"Error Acquiring Token:\r\n{msalex}");
+					return;
+				}
+			}
+
+			if (authResult == null) return; 
+			
+			var httpClient = new System.Net.Http.HttpClient();
+			System.Net.Http.HttpResponseMessage response;
+			try {
+				var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, graphAPI_Me);
+				//Add the token in Authorization header
+				request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+				response = await httpClient.SendAsync(request);
+				var content = await response.Content.ReadAsStringAsync();
+				var resultDic = JsonConvert.DeserializeObject<Dictionary<string, object>>(content);
+				name = resultDic["displayName"].ToString().Split(' ')[0];
+			} catch (Exception ex) {
+				MessageBox.Show(ex.ToString());
+				return;
+			}
+
+
+			
+		}
+	}
+}
