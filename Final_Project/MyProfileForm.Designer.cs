0e89e1ff61e4c4cf0fed38c2b13e5ecfd613411d
HenryCapyy <henry930619@gmail.com>
2023-12-06 02:47:34 +0800

    Modify MyProfileForm attribute

--- Final_Project/MyProfileForm.Designer.cs
+++ Final_Project/MyProfileForm.Designer.cs
@@ -219,8 +219,8 @@
             // 
             // MyProfileForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackgroundImage = global::Final_Project.Properties.Resources.Group_28;
             this.ClientSize = new System.Drawing.Size(1264, 711);
             this.Controls.Add(this.profile_photo);
